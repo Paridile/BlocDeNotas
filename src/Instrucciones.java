69dfe38bb3b6976250935c000d1fcf7567c90cd3
SirZempo <uaemexzempo@gmail.com>
2020-10-28 13:02:21 -0600

    Agregue las indicaciones tal cual las dio el profesor.

--- src/Instrucciones.java
+++ src/Instrucciones.java
@@ -1,7 +1,7 @@
 /*
  * BLOC DE NOTAS
  * Crear un un bloc de notas tomando en cuenta los archivo de menu archivo, menu editar
- * y menu ayuda; el menu ayuda debe terner funcionando las opciones nuevo, abrir, guardar
+ * y menu ayuda que dio; el menu archivo debe terner funcionando las opciones nuevo, abrir, guardar
  * guardar como y salir; El menu Editar debe tener funcionando las opciones Deshacer, 
  * cortar, copiar, pegar, eliminar, seleccionar todo y hora y fecha; El menu ayuda solo 
  * debe mostrar información de los participantes del equipo e información extra acerca
