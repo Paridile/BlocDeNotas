2f4d7737d0ea0e91e59ce9ce8cc9eb14adfed51b
SirZempo <uaemexzempo@gmail.com>
2020-10-28 17:04:51 -0600

    Correccion visual del menu

--- src/Bloc.java
+++ src/Bloc.java
@@ -15,46 +15,51 @@ public class Bloc implements ActionListener, ItemListener {
   private MenuItem mi2;
   private MenuItem mi3;
   private MenuItem mi4;
+  private MenuItem mi6;
   private CheckboxMenuItem mi5;
 
   public void go() {
-    f = new Frame("Menu");
+    f = new Frame("Bloc de Notas z.1");
     mb = new MenuBar();
-    m1 = new Menu("File");
-    m2 = new Menu("Edit");
-    m3 = new Menu("Help");
+    m1 = new Menu("Archivo");
+    m2 = new Menu("Edicion");
+    m3 = new Menu("Ayuda");
     mb.add(m1);
     mb.add(m2);
     mb.setHelpMenu(m3);
     f.setMenuBar(mb);
 
-    mi1 = new MenuItem("New");
-    mi2 = new MenuItem("Save");
-    mi3 = new MenuItem("Load");
-    mi4 = new MenuItem("Quit");
+    mi1 = new MenuItem("Nuevo");
+    mi2 = new MenuItem("Abrir");
+    mi3 = new MenuItem("Guardar");
+    mi4 = new MenuItem("Guardar como...");
+    mi6 = new MenuItem("Salir");
     mi1.addActionListener(this);
     mi2.addActionListener(this);
     mi3.addActionListener(this);
     mi4.addActionListener(this);
+    mi6.addActionListener(this);
+    
     m1.add(mi1);
     m1.add(mi2);
     m1.add(mi3);
-    m1.addSeparator();
     m1.add(mi4);
+    m1.addSeparator();
+    m1.add(mi6);
 
     mi5 = new CheckboxMenuItem("Persistent");
     mi5.addItemListener(this);
     m1.add(mi5);
 
-    f.setSize(200,200);
+    f.setSize(500,500);
     f.setVisible(true);
   }
 
   public void actionPerformed( ActionEvent ae) {
-    System.out.println("Opci�n \"" + 
+    System.out.println("Opcion \"" + 
         ae.getActionCommand() + "\" elegida.");
 
-    if (ae.getActionCommand().equals("Quit")) {
+    if (ae.getActionCommand().equals("Salir")) {
       System.exit(0);
     }
   }
