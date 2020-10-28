f3f017acf6e6fbcf98ab14dcb1148797c216013a
SirZempo <uaemexzempo@gmail.com>
2020-10-28 12:59:24 -0600

    Agregue las indicaciones tal cual las dio el profesor.

---
+++ src/Instrucciones.java
@@ -0,0 +1,18 @@
+/*
+ * BLOC DE NOTAS
+ * Crear un un bloc de notas tomando en cuenta los archivo de menu archivo, menu editar
+ * y menu ayuda; el menu ayuda debe terner funcionando las opciones nuevo, abrir, guardar
+ * guardar como y salir; El menu Editar debe tener funcionando las opciones Deshacer, 
+ * cortar, copiar, pegar, eliminar, seleccionar todo y hora y fecha; El menu ayuda solo 
+ * debe mostrar información de los participantes del equipo e información extra acerca
+ * del proyecto.
+ * El bloc de notas debe ser creado en swing o awt.
+ */
+
+/**
+ *
+ * @author Uriel
+ */
+public class Instrucciones {
+    
+}
