bdd54bbb0bc7f886bb6c76560cc66a4f41073352
Pablo Rios <pablorios1603@gmail.com>
2020-10-28 11:12:50 -0600

    commit inicial

---
+++ build/classes/blocdenotas/Ventana.rs
@@ -0,0 +1,6 @@
+ceis.cs.umss.edu.bo.Ventana
+ceis.cs.umss.edu.bo.Ventana$5
+ceis.cs.umss.edu.bo.Ventana$4
+ceis.cs.umss.edu.bo.Ventana$3
+ceis.cs.umss.edu.bo.Ventana$2
+ceis.cs.umss.edu.bo.Ventana$1
