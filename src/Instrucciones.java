e562a1bf41b3fee02125ea2c5590908b0ae359c7
Tield <eithel_agustin@hotmail.com>
2020-10-30 16:03:00 -0600

    Update Instrucciones.java

--- src/Instrucciones.java
+++ src/Instrucciones.java
@@ -23,8 +23,8 @@ import javax.swing.JFileChooser;
  * BLOC DE NOTAS
  * Crear un un bloc de notas tomando en cuenta los archivo de menu archivo, menu editar
  * y menu ayuda que dio; el menu archivo debe terner funcionando las opciones nuevo, abrir, guardar
- * guardar como y salir; El menu Editar debe tener funcionando las opciones Deshacer, 
- * cortar, copiar, pegar, eliminar, seleccionar todo y hora y fecha; El menu ayuda solo 
+ * guardar como y salir; El menu Editar debe tener funcionando las opciones Deshacer,
+ * cortar, copiar, pegar, eliminar, seleccionar todo y hora y fecha; El menu ayuda solo
  * debe mostrar información de los participantes del equipo e información extra acerca
  * del proyecto.
  * El bloc de notas debe ser creado en swing o awt.
@@ -34,13 +34,13 @@ public class Instrucciones {
     public static void nuevo(TextArea ta){
         ta.setText(" ");
     }
-    
+
     public static void abrir(Frame fr,TextArea ta){
         Scanner entrada = null;
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.showOpenDialog(fileChooser);
         try {
-            String ruta = fileChooser.getSelectedFile().getAbsolutePath();  
+            String ruta = fileChooser.getSelectedFile().getAbsolutePath();
             File f = new File(ruta);
             String nombre = fileChooser.getName(f);
             entrada = new Scanner(f);
@@ -62,28 +62,35 @@ public class Instrucciones {
             }
         }
     }
-    
+
     public static void salir(){
-        System.exit(0);
+      public static void salir(){
+    int resultado;
+    resultado = JOptionPane.showConfirmDialog(null,
+            "Estas seguro que deseas salir sin guardar?",
+            "Confirmar", JOptionPane.YES_NO_CANCEL_OPTION);
+    if (resultado == JOptionPane.YES_OPTION) System.exit(0);
+}
+
     }
-    
+
     public static void fechaYHora(TextArea ta){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-        LocalDateTime now = LocalDateTime.now();  
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
         ta.append(dtf.format(now) + " ");
     }
-    
+
     public static void guardar(TextArea ta) {
         System.out.println("Estas Guardando");
         String nombreArchivo = "Prueba";/*ta.getText();*///Cambiarlo por el nombre del archivo
         String carpeta = System.getProperty("user.dir");
         String rutaCompleta = carpeta + "/" + nombreArchivo + ".txt";
-        FileWriter ubicacion = null;      
+        FileWriter ubicacion = null;
         try {
             ubicacion = new FileWriter(rutaCompleta);
         } catch (IOException ex) {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
-        }      
+        }
         BufferedWriter escritor = new BufferedWriter(ubicacion);
         try {
             escritor.write(ta.getText());//RECOJE LO QUE HAY EN EL AREA DE TEXTO
@@ -96,11 +103,11 @@ public class Instrucciones {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public static void seleccionarTodo(TextArea ta) {
         ta.selectAll();
     }
-    
+
     public static void copiar (TextArea ta) {
         String seleccionado = ta.getSelectedText();
         if(seleccionado != null){
@@ -109,7 +116,7 @@ public class Instrucciones {
             clipboard.setContents(stringSelection, null);
         }
     }
-    
+
     public static void pegar(TextArea ta)  {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         if(clipboard != null){
@@ -120,7 +127,7 @@ public class Instrucciones {
                 ta.append(textopegado);
             } catch (UnsupportedFlavorException | IOException ex) {
                 Logger.getLogger(Instrucciones.class.getName()).log(Level.SEVERE, null, ex);
-            } 
-        } 
+            }
+        }
     }
 }
